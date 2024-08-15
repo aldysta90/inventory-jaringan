84ea9b0b80cf6bda4173e37e75274f07455f8477
Aldi Alfiansyah <aldialfi2309@bsi.ac.id>
2024-08-15 16:57:44 +0700

    first commit

---
+++ component/core/footer.php
@@ -0,0 +1,20 @@
+ <?php
+    error_reporting(0);
+    include 'configuration/config_connect.php';
+    $queryback = "SELECT * FROM backset";
+    $resultback = mysqli_query($conn, $queryback);
+    $rowback = mysqli_fetch_assoc($resultback);
+    $footer = $rowback['footer'];
+
+
+
+    ?>
+ <footer class="main-footer">
+     <!-- <div class="pull-right hidden-xs">
+         <b>Version</b> 2.0.0.1
+     </div> -->
+     <!-- <strong>Copyright ©2019 IDWares.</strong> All rights -->
+     <!-- reserved.  -->
+     <?php echo $footer; ?>
+     </div>
+ </footer>
